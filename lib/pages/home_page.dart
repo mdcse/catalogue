c785325e3bba570684df8695feb71b62275401b2
Danish Ikbal <mdcse786@gmail.com>
2022-09-10 11:29:28 +0530

    hi

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:catalogue/widgets/drawer.dart';
 
 class HomePage extends StatelessWidget {
-  final int days = 30;
-  final String name = "Codepur";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
